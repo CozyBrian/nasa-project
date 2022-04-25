c6f25d7d6b826d68115059a0c16e66940c977c86
CozyBrian <dragonbrian33@gmail.com>
2022-04-25 18:14:19 +0000

    feat: planets

---
+++ server/src/server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const app = require('./app');
+
+PORT = process.env.PORT || 4000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
