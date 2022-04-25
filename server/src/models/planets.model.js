1801af1d12bdded1c22160499e122f001a0c8def
CozyBrian <dragonbrian33@gmail.com>
2022-04-25 18:46:58 +0000

    feat: models

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,31 @@
-const planets = [];
+const parse = require("csv-parser");
+const fs = require("fs");
+
+const habitablePlanets = [];
+
+function isHabitable(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6; 
+}
+
+fs.createReadStream('kepler_data2.csv')
+  .pipe(parse())
+  .on('data', (data) => {
+    if (isHabitable(data)) {
+      habitablePlanets.push(data); 
+    }
+  })
+  .on('error', (err) => {
+    console.log(err);
+  })
+  .on('end', () => {
+    console.log("Done!");
+    console.log(habitablePlanets.map((planet) => {
+      return planet['kepler_name'];
+    }));
+    console.log(`${habitablePlanets.length} habitable planets found`);
+  });
+
 
 module.exports = planets;
